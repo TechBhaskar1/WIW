e3737f8ef6be7487fe07bf706f0aa4be572b358a
TechBhaskar1 <81464944+TechBhaskar1@users.noreply.github.com>
2024-03-31 23:28:11 +0530

    Add files via upload

---
+++ static/home.js
@@ -0,0 +1,61 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDEX-z_A9NhtZHQTxOZpTIhNlT9VRBAt1s",
+    authDomain: "genderequality-a2003.firebaseapp.com",
+    databaseURL: "https://genderequality-a2003-default-rtdb.firebaseio.com",
+    projectId: "genderequality-a2003",
+    storageBucket: "genderequality-a2003.appspot.com",
+    messagingSenderId: "943614717050",
+    appId: "1:943614717050:web:e22d32fed24eddff62dfc3"
+  };
+  
+  
+  
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+
+var parentDiv = document.getElementById("parent")
+var usernameL = localStorage.getItem('username');
+var genderL = localStorage.getItem('gender');
+var username = document.getElementById('username');
+username.innerText = "Username :" + usernameL;
+
+db.ref("product").once('value')
+  .then((snapshot)=>{
+    snapshot.forEach((childSnapshot)=>{
+        const product = childSnapshot.val();
+        divX = '<div class="main"><h1 id="topic">'+product.product+'</h1><img id="image"  width="80%" src="'+product.image+ '"><br><br> <p id="price">'+product.price+'</p><label>Product ID :</label><p id = "ID">'+product.id+'</p><button id="buy" type="button" onclick="buy()">Buy Now</button></div>';
+        parentDiv.innerHTML += divX;
+    })
+    const mainDiv = document.querySelectorAll('.main');
+
+    mainDiv.forEach(div => {
+      div.addEventListener('click' , function(){
+        const idText = this.querySelector('#ID');
+        if(idText){
+         const mainID = idText.textContent;
+         console.log(mainID)
+         localStorage.setItem("ID" , mainID);
+        }
+      })
+    })
+    
+  })
+
+
+function create(){
+    if(genderL ==="male"){
+        window.location.href = "../templates/serviceM.html";
+    }
+    else if(genderL ==="female"){
+        window.location.href = "../templates/serviceF.html";
+    }
+}
+
+function buy(){
+    window.location.href = "../templates/work.html";
+}
+
+
+
+
---
+++ static/signup.js
@@ -0,0 +1,85 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDEX-z_A9NhtZHQTxOZpTIhNlT9VRBAt1s",
+  authDomain: "genderequality-a2003.firebaseapp.com",
+  databaseURL: "https://genderequality-a2003-default-rtdb.firebaseio.com",
+  projectId: "genderequality-a2003",
+  storageBucket: "genderequality-a2003.appspot.com",
+  messagingSenderId: "943614717050",
+  appId: "1:943614717050:web:e22d32fed24eddff62dfc3"
+};
+
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+
+//to create account
+function register() {
+  var userName = document.getElementById("username").value;
+  var phone = document.getElementById("phone").value;
+  var email = document.getElementById("mail").value;
+  var password = document.getElementById("pw").value;
+  var gender = document.querySelector('input[name="gender"]:checked');
+  var genderValue = gender.value;
+
+  if (!userName || !phone || !email || !password || !genderValue) {
+    window.alert("All the fields are required");
+    return;
+  }
+  const data = {
+    name: userName,
+    phone: phone,
+    email: email,
+    password: password,
+    gender: genderValue
+  };
+  console.log(data);
+
+  db.ref('users').push(data);
+
+  localStorage.setItem('username' , userName);
+  localStorage.setItem('gender', genderValue);
+  window.location.href = '../templates/home.html';
+}
+
+//to login into webpage
+function login() {
+  var userName = document.getElementById("username").value;
+  var phone = document.getElementById("phone").value;
+  var email = document.getElementById("mail").value;
+  var password = document.getElementById("pw").value;
+  var gender = document.querySelector('input[name="gender"]:checked');
+  var genderValue = gender.value;
+
+  if (!userName || !phone || !email || !password || !genderValue) {
+    window.alert("All the fields are required");
+    return;
+  }
+
+  db.ref('users').once('value')
+    .then((snapshot) => {
+      snapshot.forEach((childSnapshot) => {
+        const user = childSnapshot.val();
+        if (user.name == userName && user.phone == phone && user.email == email && user.password == password && user.gender == genderValue) {
+          window.location.href = "../templates/home.html";
+        }
+        else {
+          window.alert("Invalid input!!");
+          return;
+        }
+      }
+      )
+    })
+}
+
+//to navigate to signup page to create new account
+function createN() {
+  window.location.href = "../templates/signup.html"
+}
+
+//to navigate to login page to login into existing account
+function loginN() {
+  window.location.href = "../templates/login.html"
+}
+
